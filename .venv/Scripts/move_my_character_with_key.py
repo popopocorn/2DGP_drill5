201dfdd0960ebc79c2006bfb36c1fd1f9dd11a5c
popopocorn <popcorn375@naver.com>
2024-09-30 16:36:28 +0900

    구조 수정 및 리소스 가져오기

--- .venv/Scripts/move_my_character_with_key.py
+++ .venv/Scripts/move_my_character_with_key.py
@@ -1,53 +1,60 @@
 from pico2d import *
 
-class Player():
-    def __init__(self):
-        self.player_x=800//2
-        self.player_y=90
-        #self.player_idle = load_image('Sword_Idle_full.png')
-        #self.player_run = load_image('Sword_Run_full.png')
-        self.player_flag='idle'
-        player_dir={(0, 1), (0, -1), (-1, 0), (1, 0)} #상하좌우
-    def set_flag(self):
-        global running
-        events = get_events()
-        for event in events:
-            if event.type == SDL_QUIT:
-                running = False
-            elif event.type == SDL_KEYDOWN:
-                if event.key == SDLK_RIGHT:
-                    pass
-                elif event.key == SDLK_LEFT:
-                    pass
-                elif event.key == SDLK_UP:
-                    pass
-                elif event.key == SDLK_DOWN:
-                    pass
-                elif event.key == SDLK_ESCAPE:
-                    running = False
 
-    def do_act(self):
-        if self.player_flag == 'idle':
-            pass
-        elif self.player_flag == 'right':
-            pass
-        elif self.player_flag == 'left':
-            pass
-        elif self.player_flag == 'up':
-            pass
-        elif self.player_flag == 'down':
-            pass
-    def play_animation(self, dir):
+
+def set_flag():
+    global running, player_flag, frame
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                player_flag = 'right'
+                frame = 0
+            elif event.key == SDLK_LEFT:
+                player_flag = 'left'
+                frame = 0
+            elif event.key == SDLK_UP:
+                player_flag = 'up'
+                frame = 0
+            elif event.key == SDLK_DOWN:
+                player_flag = 'down'
+                frame = 0
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            player_flag='idle'
+            frame=0
+def play_animation(dir, frame):
+    clear_canvas()
+    self.ground.draw(400, 300)
+
+    update_canvas()
+running = True
+frame = 0
+open_canvas()
+player_x=800//2
+player_y=600//2
+player_idle = load_image('Sword_Idle_full.png')
+player_run = load_image('Sword_Run_full.png')
+ground = load_image('TUK_GROUND.png')
+player_flag='idle'
+player_dir={(0, 1), (0, -1), (-1, 0), (1, 0)} #상하좌우
+
+while running:
+    set_flag()
+    if(player_flag=='idle'):
+        pass
+    elif(player_flag=='up'):
+        pass
+    elif(player_flag=='down'):
+        pass
+    elif(player_flag=='left'):
+        pass
+    elif(player_flag=='right'):
         pass
 
-
-
-
-player = Player()
-open_canvas()
-running = True
-while running:
-    player.set_flag()
-    player.do_act()
+    delay(0.05)
 
 
